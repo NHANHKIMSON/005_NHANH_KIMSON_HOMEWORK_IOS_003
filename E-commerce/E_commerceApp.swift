5f916d4c3c29b042eadfd1aa91c2d850368dc53a
nhanhkimson.biu@gmail.com <nhanhkimson.biu@gmail.com>
2025-08-25 16:09:14 +0700

    Initial Commit

---
+++ E-commerce/E_commerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerceApp.swift
+//  E-commerce
+//
+//  Created by Apple on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct E_commerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ E-commerceTests/E_commerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerceTests.swift
+//  E-commerceTests
+//
+//  Created by Apple on 8/25/25.
+//
+
+import Testing
+@testable import E_commerce
+
+struct E_commerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
